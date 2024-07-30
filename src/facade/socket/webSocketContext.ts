4b9ef19ecdeee3dea84500a92c588decd26b00f9
RXS\mmach-c <mmach-c@rxsinfo.com>
2024-07-30 22:00:42 +0200

    save terminal row

--- src/facade/socket/webSocketContext.ts
+++ src/facade/socket/webSocketContext.ts
@@ -1,7 +1,7 @@
 import { useAtom, useSetAtom } from "jotai";
 import React, { useEffect } from "react";
-import { useParams } from 'react-router';
 import useWebSocket from "react-use-websocket";
+import { historyAtom } from "src/pages/programs/runProgram/store";
 import { currentStepAtom } from "src/sections/programs/diagram/store";
 import { WebSocketMessageType } from "src/types/program/programType";
 
@@ -15,6 +15,9 @@ export const WebsocketContext = ({ children, workId }: Prop) => {
 
     const { sendMessage, lastMessage, readyState } = useWebSocket(`wss://polsoft2000api.azurewebsites.net/ws?workId=${workId}`);
     const setCurrentStep = useSetAtom(currentStepAtom);
+    const setHistoryAtom = useSetAtom(historyAtom);
+    const [history] = useAtom(historyAtom);
+
     useEffect(() => {
         if (lastMessage !== null) {
             console.log(lastMessage)
@@ -22,11 +25,16 @@ export const WebsocketContext = ({ children, workId }: Prop) => {
                 const obj = JSON.parse(lastMessage.data) as WebSocketMessageType
                 console.log(obj);
                 setCurrentStep(String(obj.StepId));
+                setHistoryAtom([...history, obj]);
+
             } catch (er) {
                 console.log(undefined)
             }
         }
-        return () => setCurrentStep(null)
+        return () => {
+            setCurrentStep(null)
+            setHistoryAtom([])
+        }
     }, [lastMessage, workId, setCurrentStep]);
 
     return children
