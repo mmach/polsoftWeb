b4989b649fe648a18e6b867cdf451be1dcd64f48
Pawel <pawel.k.pyka@gmail.com>
2024-07-30 16:12:37 +0200

    Remove unused imports in home-view.tsx

--- src/sections/_home/view/home-view.tsx
+++ src/sections/_home/view/home-view.tsx
@@ -1,9 +1,6 @@
 import { useScroll } from 'framer-motion';
-
 import { _pricingHome } from 'src/_mock';
-
 import ScrollProgress from 'src/components/scroll-progress';
-
 import HomeHero from '../home-hero';
 import HomeFAQs from '../home-faqs';
 import HomeNewStart from '../home-new-start';
